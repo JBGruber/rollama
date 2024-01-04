dae5667fa7d36e19649bf1b4bdf5267a4c52cbc0
JBGruber <JohannesB.Gruber@gmail.com>
2024-01-04 01:17:28 +0100

    adds delete_model

--- R/models.r
+++ R/models.r
@@ -1,9 +1,16 @@
-#' Pull a new model or show more information about it
+#' Pull, show and delete models
 #'
-#' @param model name of the model. "llama2" by default.
+#' @details
+#' - `pull_model()`: downloads model
+#' - `show_model()`: displays information about a local model
+#' - `delete_model()`: deletes local model
+#'
+#' @param model name of the model. Defaults to "llama2" when `NULL` (except in
+#'   `delete_model`).
 #' @inheritParams query
 #'
-#' @return (invisible) a tibble with information about the model
+#' @return (invisible) a tibble with information about the model (except in
+#'   `delete_model`)
 #' @export
 #'
 #' @examples
@@ -20,7 +27,7 @@ pull_model <- function(model = NULL, server = NULL) {
   httr2::request(server) |>
     httr2::req_url_path_append("/api/pull") |>
     httr2::req_body_json(list(name = model)) |>
-    httr2::req_perform_stream(callback = pgrs, buffer_kb = 0.1L)
+    httr2::req_perform_stream(callback = pgrs, buffer_kb = 0.1)
 
   the$str_prgs <- NULL
 
@@ -41,6 +48,22 @@ show_model <- function(model = NULL, server = NULL) {
     httr2::resp_body_json() |>
     purrr::list_flatten(name_spec = "{inner}") |>
     tibble::as_tibble()
+}
+
+
+#' @rdname pull_model
+#' @export
+delete_model <- function(model, server = NULL) {
+
+  if (is.null(server)) server <- getOption("rollama_server", default = "http://localhost:11434")
+
+  httr2::request(server) |>
+    httr2::req_url_path_append("/api/delete") |>
+    httr2::req_method("DELETE") |>
+    httr2::req_body_json(list(name = model)) |>
+    httr2::req_perform()
+
+  cli::cli_progress_message("{cli::col_green(cli::symbol$tick)} {model} removed")
 }
 
 
