a8e2ca015096d3238f7fa1bc242845468ae9c0ef
JBGruber <JohannesB.Gruber@gmail.com>
2024-01-06 15:09:59 +0100

    forgot to skip test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,5 +1,6 @@
 test_that("verbose", {
   expect_no_message({
+    skip_if_not(ping_ollama(silent = TRUE))
     op <- options("rollama_verbose" = FALSE)
     on.exit(options(op), add = TRUE, after = FALSE)
     query(q = "test", screen = FALSE)
