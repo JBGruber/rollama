b7cece719fa56816babde64a262ac205e5ec9ce8
JBGruber <johannesb.gruber@gmail.com>
2024-06-02 19:52:29 +0200

    address #16

--- R/embedding.r
+++ R/embedding.r
@@ -28,52 +28,40 @@ embed_text <- function(text,
                        verbose = getOption("rollama_verbose", default = interactive())) {
 
   if (is.null(model)) model <- getOption("rollama_model", default = "llama3")
+  check_model_installed(model)
   if (is.null(server)) server <- getOption("rollama_server", default = "http://localhost:11434")
-  if (verbose) cli::cli_progress_bar(
-    total = length(text),
+
+  pb <- FALSE
+  if (verbose) pb <- list(
     format = "{cli::pb_spin} embedding text {cli::pb_current} / {cli::pb_total} ({cli::pb_rate}) {cli::pb_eta}",
     format_done = "{cli::col_green(cli::symbol$tick)} embedded {cli::pb_total} texts {cli::col_silver('[', cli::pb_elapsed, ']')}",
-    clear = FALSE,
-    .envir = the
+    clear = FALSE
   )
 
-  out <- purrr::map(seq_along(text), function(i) {
+  reqs <- purrr::map(text, function(t) {
 
     req_data <- list(model = model,
-                     prompt = text[i],
+                     prompt = t,
                      stream = FALSE,
                      model_params = model_params) |>
-      purrr::compact()
+      purrr::compact() |>
+      make_req(server = server,
+               endpoint = "/api/embeddings",
+               perform = FALSE)
+  })
 
-    if (verbose) {
-      # cli::cli_progress_step("{cli::pb_spin} {model} is embedding text {i}")
-      rp <- callr::r_bg(make_req,
-                        args = list(req_data = req_data,
-                                    server = server,
-                                    endpoint = "/api/embeddings"),
-                        package = TRUE)
-      while (rp$is_alive()) {
-        cli::cli_progress_update(inc = 0L, .envir = the)
-        Sys.sleep(2 / 100)
-      }
-      resp <- rp$get_result()
-      cli::cli_progress_update(.envir = the)
+  resps <- httr2::req_perform_parallel(reqs, progress = pb)
 
+  out <- purrr::map(resps, function(resp) {
+    if (httr2::resp_content_type(resp) == "application/json") {
+      emd <- httr2::resp_body_json(resp) |>
+        purrr::pluck("embedding")
+      names(emd) <- paste0("dim_", seq_along(emd))
+      tibble::as_tibble(emd)
     } else {
-      resp <- make_req(req_data, server, "/api/embeddings")
+      cli::cli_alert_danger("Request did not return embeddings")
     }
-
-    if (!is.null(resp$error)) {
-      if (grepl("model.+not found, try pulling it first", resp$error)) {
-        resp$error <- paste(resp$error, "with {.code pull_model(\"{model}\")}")
-      }
-      cli::cli_abort(resp$error)
-    }
-    names(resp$embedding) <- paste0("dim_", seq_along(resp$embedding))
-    tibble::as_tibble(resp$embedding)
   }) |>
     dplyr::bind_rows()
-
-  if (verbose) cli::cli_progress_done(.envir = the)
   return(out)
 }
