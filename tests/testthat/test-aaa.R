10dfce8c7cdae315394a5e1c7e2e5b5037153e17
JBGruber <johannesb.gruber@gmail.com>
2024-10-20 10:00:52 +0200

    add more tests

---
+++ tests/testthat/test-aaa.R
@@ -0,0 +1,6 @@
+# slightly out of place, but we don't want to pull unnecessary models and this
+# should come before the first test
+test_that("Auto pull model", {
+  expect_true(check_model_installed(getOption("rollama_model", default = "llama3.1"),
+                                    auto_pull = TRUE))
+})
--- tests/testthat/test-chat.R
+++ tests/testthat/test-chat.R
@@ -21,6 +21,26 @@ test_that("Test chat", {
   }, 0L)
 })
 
+test_that("Test inputs", {
+  skip_if_not(ping_ollama(silent = TRUE))
+  # text
+  expect_message(query("test"), ".")
+  # text + image (I don't want to pull a different model for this, the API still
+  # works with models that can't handle images)
+  query("test",
+        images = system.file("extdata", "logo.png", package = "rollama"))
+  # data.frame
+  expect_message(query(data.frame(role = "user", content = "test")), ".")
+  expect_error(query(data.frame(role = "system", content = "test")),
+               "at.least.one.user.message")
+  # list of data.frames
+  l <- rep(list(data.frame(role = "user", content = "test")), 5)
+  answers <- query(l, screen = FALSE)
+  expect_length(answers, 5)
+  expect_type(answers, "list")
+  expect_type(answers[[1]], "list")
+})
+
 test_that("Test output parameter", {
   expect_s3_class(
     query("Please only say 'yes'", output = "httr2_request")[[1]],
